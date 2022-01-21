34fc5d927c669dc08b7070e781aff29dea538c03
Jean <nguyen.jean@me.com>
2022-01-21 10:15:44 +0100

    setup react project and add first component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Footer from './components/footer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
